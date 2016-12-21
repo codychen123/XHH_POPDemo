4f81159350a6550934762542a327632b58403fdd
ithuihui <chhhithuihui@gmail.com>
2016-12-21 11:32:04 +0800

    Initial Commit

---
+++ XHH_POPDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XHH_POPDemo
+//
+//  Created by xiaohuihui on 2016/12/21.
+//  Copyright © 2016年 30-陈辉华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
