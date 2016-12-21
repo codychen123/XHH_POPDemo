f8555946653a25820b6c044bd595e1f942101195
ithuihui <chhhithuihui@gmail.com>
2016-12-21 13:23:56 +0800

    完成popDemo

--- XHH_POPDemo/ViewController.m
+++ XHH_POPDemo/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <POP.h>
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+/** 当前图片下标 */
+@property (assign, nonatomic) NSInteger index;
 
 @end
 
@@ -16,9 +21,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    /** 裁剪为圆形 */
+    self.imageView.layer.cornerRadius = 120;
+    self.imageView.layer.masksToBounds = YES;
+    [self addRotateAnimation];
 }
 
+/** 添加旋转动画 */
+- (void)addRotateAnimation {
+    POPBasicAnimation *rotationAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerRotation];
+    rotationAnimation.beginTime = CACurrentMediaTime();
+    rotationAnimation.toValue = @(1000);
+    rotationAnimation.duration = 1000;
+    [self.imageView.layer pop_addAnimation:rotationAnimation forKey:nil];
+}
+
+/** 添加缩放动画 */
+- (void)addAnimaition {
+    /** 缩放动画 */
+    POPBasicAnimation *scaleAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+    scaleAnimation.toValue = [NSValue valueWithCGPoint:CGPointMake(0.7, 0.7)];
+    scaleAnimation.duration = 0.3;
+    [self.imageView.layer pop_addAnimation:scaleAnimation forKey:nil];
+    scaleAnimation.completionBlock = ^(POPAnimation *animation, BOOL finished) {
+        POPBasicAnimation *scaleAnimationTwo = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+        scaleAnimationTwo.toValue = [NSValue valueWithCGPoint:CGPointMake(1, 1)];
+        scaleAnimationTwo.duration = 0.3;
+        [self.imageView.layer pop_addAnimation:scaleAnimationTwo forKey:nil];
+    };
+}
+
+- (IBAction)preButton:(id)sender {
+}
+
+- (IBAction)nextButton:(id)sender {
+    [self addAnimaition];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
